e8bec944a47b076ffd662cc32646e3d0a93317ad
rnairopine <48428111+rnairopine@users.noreply.github.com>
2019-03-12 10:26:09 +0400

    Committing the src

---
+++ src/main/java/com/microservices/demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.microservices.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/greet")
+	public String greet() {
+		return "Hello world!";
+	}
+}
